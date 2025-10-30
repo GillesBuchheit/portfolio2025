db8db2efc31187695bcf3a7817ed2ebfe477e2d2
Gilles Buchheit <buchheit.gilles@gmail.com>
2025-10-30 14:18:07 +0100

    feat: enhance HeroLanding component with menu and smooth scroll functionality

--- app/components/landingPage/HeroLanding.jsx
+++ app/components/landingPage/HeroLanding.jsx
@@ -3,14 +3,58 @@
 import { motion, AnimatePresence } from "motion/react";
 import { Button } from "@/components/ui/button";
 import { useState, useEffect } from "react";
-import { ArrowRight } from "lucide-react";
+import { ArrowDown, ArrowRight, MenuSquareIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 
 const technologies = ["React", "Java", "Next.js", "TypeScript"];
 
+function Menu({ isOpen, setIsOpen }) {
+  return (
+    <AnimatePresence mode="wait">
+      {isOpen && (
+        <motion.div
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -100 }}
+          transition={{ duration: 0.5 }}
+          className="fixed top-0 left-0 w-full h-full z-30 flex items-center justify-center  bg-[#f5f5f0]"
+        >
+          <motion.div className="flex flex-col gap-2 ove">
+            <Button
+              initial={{ opacity: 0, y: -100 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -100 }}
+              transition={{ duration: 0.5 }}
+            >
+              <MenuSquareIcon className="w-6 h-6" />
+            </Button>
+            <Button
+              initial={{ opacity: 0, y: -100 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -100 }}
+              transition={{ duration: 0.5 }}
+            >
+              <MenuSquareIcon className="w-6 h-6" />
+            </Button>
+            <Button
+              initial={{ opacity: 0, y: -100 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -100 }}
+              transition={{ duration: 0.5 }}
+            >
+              <MenuSquareIcon className="w-6 h-6" />
+            </Button>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
+
 export default function HeroLanding() {
   const [currentTechIndex, setCurrentTechIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -22,10 +66,23 @@ export default function HeroLanding() {
   return (
     <section
       id="accueil"
-      className="relative min-h-screen h-screen flex flex-col overflow-hidden bg-[#f5f5f0]"
+      className="relative min-h-screen h-screen flex flex-col overflow-hidden bg-[#f5f5f0] px-4 sm:px-6 md:px-12 lg:px-20"
     >
-      <div className="flex flex-1 pt-8 sm:pt-12 md:pt-16 px-4 sm:px-6 md:px-12 lg:px-20 justify-between items-center lg:justify-between justify-center">
-        <div className="flex flex-col justify-center w-full lg:max-w-3xl max-w-full items-center lg:items-start">
+      <Menu isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div className=" text-white flex items-center justify-between w-full fixed top-0 left-0 px-4 sm:px-6 md:px-12 lg:px-20 py-10 z-50">
+        <span className="bg-[#0A0100] p-5 w-15 h-15 flex items-center justify-center font-erstoria text-2xl sm:text-3xl md:text-3xl ">
+          GB.
+        </span>
+        <Button
+          onClick={() => setIsOpen(!isOpen)}
+          size="icon-lg"
+          // variant="secondary"
+        >
+          <MenuSquareIcon size={24} />
+        </Button>
+      </div>
+      <div className="flex flex-1 pt-8 sm:pt-12 md:pt-16  items-center lg:justify-between justify-center gap-8">
+        <div className="flex flex-col justify-center w-full lg:w-3/5 max-w-full items-center lg:items-start">
           <div className="flex items-center gap-2">
             <span className="h-px w-8 sm:w-14 bg-[#0A0100]"></span>
             <h1 className="text-xs sm:text-sm lg:text-lg font-erstoria text-[#0A0100]/60 whitespace-nowrap">
@@ -33,10 +90,10 @@ export default function HeroLanding() {
             </h1>
             <span className="h-px w-8 sm:w-14 bg-[#0A0100]"></span>
           </div>
-          <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-[#e61f00] font-erstoria mt-2 text-center lg:text-left">
+          <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-8xl text-[#e61f00] font-erstoria mt-2 text-center lg:text-left">
             Développeur web
           </h2>
-          <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-black font-erstoria flex flex-wrap items-center justify-center lg:justify-start min-h-[1.2em] gap-2">
+          <div className="text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-8xl text-black font-erstoria flex flex-wrap items-center justify-center lg:justify-start min-h-[1.2em] gap-2">
             <span className="whitespace-nowrap">Fullstack</span>
             <span>|</span>
             <AnimatePresence mode="wait">
@@ -74,15 +131,15 @@ export default function HeroLanding() {
           </div>
         </div>
         <Image
-          src="/abstract2.jpg"
+          src="/hero_banner.jpg"
           alt="Illustration"
-          width={400}
-          height={500}
-          className="hidden lg:block object-cover max-w-[300px] xl:max-w-[400px] h-auto"
+          width={1920}
+          height={1097}
+          className="hidden lg:block object-cover lg:w-2/5 h-auto rotate-90 xl:rotate-0"
         />
       </div>
 
-      <div className="grid grid-cols-2 sm:flex sm:justify-between px-4 sm:px-6 md:px-12 p-4 sm:p-6 gap-4 sm:gap-6">
+      <div className="grid grid-cols-2 sm:flex sm:justify-between  gap-4 sm:gap-6 py-4 sm:py-6">
         <div className="flex flex-col gap-1 items-center sm:items-start">
           <span className="font-erstoria text-sm sm:text-base">
             Travaille à
@@ -97,16 +154,26 @@ export default function HeroLanding() {
             ANS D&apos;EXPERIENCE
           </span>
         </div>
-        <div className="flex flex-col gap-1 items-center sm:items-start col-span-2 sm:col-span-1">
+        <div className="flex flex-col gap-1 items-center sm:items-center col-span-2 sm:col-span-1">
           <Link
             href="#"
-            className="font-erstoria text-sm sm:text-base hover:text-[#e61f00] transition-colors"
+            className="font-erstoria text-sm sm:text-base transition-colors"
           >
             Explorer
           </Link>
-          <span className="text-xs sm:text-sm text-[#0A0100]/60">
-            METZ | NANCY | LUXEMBOURG
-          </span>
+          <Button
+            size="icon"
+            variant="ghost"
+            className="hover:bg-transparent cursor-pointer hover:text-[#0A0100]/60 hover:scale-110 transition-all duration-300"
+            onClick={() => {
+              window.scrollTo({
+                top: window.innerHeight,
+                behavior: "smooth",
+              });
+            }}
+          >
+            <ArrowDown className="w-4 h-4" />
+          </Button>
         </div>
       </div>
     </section>
